c813a247d200b4e68c45390ffc23f0f790a47d71
Victor Kimathi <vkimathi97@gmail.com>
2025-07-18 00:36:08 +0300

    python-p3-dsa-stack-lab

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,36 @@
 class Stack:
 
-    def __init__(self, items = [], limit = 100):
-        pass
+    def __init__(self, items=None, limit=100):
+        if items is None:
+            items = []
+        self.items = items.copy()
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if not self.full(): 
+            self.items.append(item)
 
     def pop(self):
-        pass
+        if self.isEmpty():
+            return None
+        return self.items.pop()
 
     def peek(self):
-        pass
-    
+        if self.isEmpty():
+            return None
+        return self.items[-1]
+
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) >= self.limit  # >= for safety
 
     def search(self, target):
-        pass
+        try:
+            return (len(self.items) - 1) - self.items.index(target)
+        except ValueError:
+            return -1
